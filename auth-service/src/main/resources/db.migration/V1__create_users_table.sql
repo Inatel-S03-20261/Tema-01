c08b08c1a2a126e039e1d48efc30b44fedee0898
migstreva <miguelstreva@outlook.com>
2026-06-02 20:12:23 -0300

    refactor: date_of_birth set to not null and removed 'if not exists'

--- auth-service/src/main/resources/db.migration/V1__create_users_table.sql
+++ auth-service/src/main/resources/db.migration/V1__create_users_table.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id UUID PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL, -- Senhas com hash/criptografia costumam precisar de espaço
-    date_of_birth DATE,
+    password VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
     role VARCHAR(20) NOT NULL,
     banned BOOLEAN NOT NULL DEFAULT FALSE,
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
